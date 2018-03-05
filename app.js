c301761e9d20cab483b4091e6818148c9a064e93
Paul Yuknewicz <paulyuk@microsoft.com>
2018-03-05 00:34:09 -0800

    Added App Insights
    SDK only.  Use environment variable to set iKey

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+
+
+// Add your instrumentation key or use the APPLICATIONINSIGHTSKEY environment variable on your production machine to start collecting data.
+var ai = require('applicationinsights');
+ai.setup(process.env.APPLICATIONINSIGHTSKEY || 'your_instrumentation_key').start();
 var http = require('http'),
     path = require('path'),
     methods = require('methods'),
